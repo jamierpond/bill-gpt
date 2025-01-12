fix causal

infer.py
@@ -6,7 +6,10 @@ from model import Transformer
 def generate(model, initial_context, max_len=256, temp=0.8):
     tokenizer = DumbTokenizer()
     for _ in range(max_len):
-        logits = model(initial_context)
+        if initial_context.size(1) > 1024:
+            # pop the first token
+            initial_context = initial_context[:, 1:]
+        logits = model(initial_context, causal=True)
         next_token = torch.multinomial(torch.softmax(logits[:, -1, :] / temp, dim=-1), 1)
         next_token = (torch.Tensor(next_token).int())
         initial_context = torch.cat([initial_context, next_token], dim=-1)
@@ -16,15 +19,22 @@ def generate(model, initial_context, max_len=256, temp=0.8):
     print()
 
 
+prompt = """
+ROMEO:
+    O, strike my face. I am the dragon
+"""
+
 if __name__ == "__main__":
     ds = TextDataset(BILL_PATH)
-    input_tokens, y = ds[5000]
-    model = Transformer(vocab_size=256)
-    model.load_state_dict(torch.load("best-model.pth", weights_only=True))
+    # input_tokens, _ = ds[5000]
+    input_tokens = ds.tokenizer.tokenize(prompt)
+    input_tokens = torch.Tensor(input_tokens).int().unsqueeze(0)
+    model = Transformer(vocab_size=ds.tokenizer.vocab_size)
+    model.load_state_dict(torch.load("model.pth", weights_only=True))
 
     model.to("cuda")
     input_tokens = input_tokens.to("cuda")
 
-    initial_context = input_tokens.unsqueeze(0)
-    out = generate(model, initial_context)
+    initial_context = input_tokens
+    out = generate(model, initial_context, max_len=int(1e18))
 

model.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 from attention import MultiHeadAttention
 
 class Transformer(nn.Module):
-    def __init__(self, vocab_size, dim=128, depth=16, heads=8, causal=False):
+    def __init__(self, vocab_size, dim=128, depth=16, heads=8):
         super().__init__()
         self.token_emb = nn.Embedding(vocab_size, dim)
 
@@ -24,9 +24,9 @@ class Transformer(nn.Module):
         self.to_logits = nn.Linear(dim, vocab_size)
 
     @staticmethod
-    def get_causal_mask(seq_len):
+    def get_causal_mask(seq_len, device):
         mask = torch.triu(torch.ones(seq_len, seq_len), diagonal=1).bool()
-        return mask
+        return mask.to(device)
 
     def forward(self, x, causal=False):
         batch_size, seq_len = x.size()
@@ -40,7 +40,7 @@ class Transformer(nn.Module):
         assert token_emb.shape == pos_emb.shape
         x = token_emb + pos_emb
 
-        mask = self.get_causal_mask(seq_len) if causal else None
+        mask = self.get_causal_mask(seq_len, x.device) if causal else None
         for sub_list in self.layers:
             assert isinstance(sub_list, nn.ModuleList)
             attn, ln1, ff1, gelu, ff2, ln2 = sub_list

train.py
@@ -1,4 +1,3 @@
-
 import tqdm
 import torch.nn.functional as F
 import torch
@@ -10,6 +9,16 @@ from data import DumbTokenizer
 from infer import generate
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+FAUSTUS = """
+Settle thy studies, Faustus, and begin
+To sound the depth of that thou wilt profess.
+Having commenced, be a divine in show,
+Yet level at the end of every art,
+And live and die in Aristotle's works.
+"""
+
 
 def analyze_predictions(model, loader):
     model.eval()
@@ -41,7 +50,7 @@ def train():
     model = Transformer(vocab_size=vocab_size)
 
     # load checkpoint
-    # model.load_state_dict(torch.load("best-model.pth", weights_only=True, map_location=device))
+    model.load_state_dict(torch.load("best-model.pth", weights_only=True, map_location=device))
     model.to(device)
     num_params = sum(p.numel() for p in model.parameters())
     print(f"Number of parameters: {num_params}")
@@ -77,7 +86,9 @@ def train():
                 torch.save(model.state_dict(), f'best-model.pth')
 
             if step % 150 == 0:
-                generate(model, x[0].unsqueeze(0))
+                faustus_tokens = dataset.tokenizer.tokenize(FAUSTUS)
+                faustus_tokens = torch.Tensor(faustus_tokens).int().unsqueeze(0).to(device)
+                generate(model, faustus_tokens)
 
             if step % 20 == 0:
                 tqdm.tqdm.write(f"Loss: {loss.item()}, Lowest: {lowest_loss}")