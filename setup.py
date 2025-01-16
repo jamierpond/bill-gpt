397d4db184970bc5805688223eebd62adeab21d5
Jamie Pond <jamiepond259@gmail.com>
2025-01-15 20:10:13 -0800

    fix setup

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@ from setuptools import setup
 
 setup(
     name="billgpt",
-    packages=["billgpt"],  # Directly specify the package
-    package_dir={"": "billgpt"},
     install_requires=[
         "torch",
         "numpy",
